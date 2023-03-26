05dca625ba201eac8ee58929ea4dc23f87eb7c8d
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2023-03-26 18:40:57 +0400

    Add ErrorModal component

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,5 +1,21 @@
 import styles from './ErrorModal.module.css';
+import Card from './Card.jsx';
+import Button from './Button.jsx';
 
-const ErrorModal = (props) => {};
+const ErrorModal = (props) => {
+  return(
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button>OK</Button>
+      </footer>
+    </Card>
+  )
+};
 
 export default ErrorModal;
